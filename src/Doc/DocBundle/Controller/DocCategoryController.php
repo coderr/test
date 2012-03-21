a172b457cbba2ec90c1e1b44054695d88277beae
terramda <terramda@gmail.com>
2012-03-21 22:09:30 +0200

    Doc bundle

--- src/Doc/DocBundle/Controller/DocCategoryController.php
+++ src/Doc/DocBundle/Controller/DocCategoryController.php
@@ -12,7 +12,7 @@ class DocCategoryController extends Controller {
     private function checkLogin() {
         $session = $this->getRequest()->getSession();
         if ($session->get('auth') !== 'in') {
-            return $this->redirect($this->generateUrl('UserUserBundle_login'));
+            return $this->redirect($this->generateUrl('NotarNotarBundle_login'));
         }
     }
 
